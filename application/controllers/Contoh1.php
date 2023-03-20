4ca5b104314adf1c245579695ae51aa80fe48fc9
aldifirmansyah <aldif459@gmail.com>
2023-03-20 18:45:25 +0700

    Tugas Pertemuan 1

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Aldi Firmansyah
+            saya tinggal di daerah jakarta
+            olahraga yang saya sukai adalah
+            Sepakbola";
+    }
+}
