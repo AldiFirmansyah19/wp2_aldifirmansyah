5f368892ce8bc516322953715999fe4f55b84df6
aldifirmansyah <aldif459@gmail.com>
2023-03-27 18:25:13 +0700

    Tugas Pertemuan 3

---
+++ application/views/view-latihan1.php
@@ -0,0 +1,15 @@
+<html>
+
+<body>
+    <title>Latihan 1</title>
+</head>
+
+<body>
+    Halo Kawan.. Yuk kita belajar web programming..!!!<br>
+    Nilai 1 = <?= $nilai1; ?>
+    Nilai 2 = <?= $nilai2; ?>
+    ini hasil dari pemodelan dengan methode penjumalahan yaitu <?=$nilai1 . " + " . $nilai2. ' = '. $hasil; ?>
+
+</body>
+
+</html>
